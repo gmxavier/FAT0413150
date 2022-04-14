1f80ccca11e74a0bf3530d3d48aaf6bc9f96f1c6
Gilberto M. Xavier <gilberto.xavier@lps.ufrj.br>
2022-04-14 19:43:17 -0300

    Create pidtest.py

---
+++ src/pidtest.py
@@ -0,0 +1,27 @@
+def pidtest(Kp, Ki, Kd, 
+            alpha = 0.02,
+            num   = [1e-1],
+            den   = [20**2, 2*0.5*20, 1],
+            theta = 0,
+            t0 = 25,
+            T  = np.arange(0, 10*20, 0.2),
+            test = True):
+  
+  Gp = Gcl = tf(num, den)
+  plt.title('Process reaction curve')
+  if test == True:
+    Gc  = tf([Kd, Kp, Ki], [alpha*Kd/Kp, 1, 0])
+    Gcl = feedback(Gc*Gp, 1)
+    plt.title('Servo control response \n Kp = {0} | Ki = {1} | Kd = {2}'.format(Kp,Ki,Kd))
+  u = np.ones(len(T))
+  u[T<=t0] = 0
+  ud = np.ones(len(T))
+  ud[T<=(t0+theta)] = 0
+  y = lsim(Gcl, ud, T)[0]
+  plt.plot(T, y, T, u)
+  plt.xlabel('Time')
+  plt.ylabel('Amplitude')
+  plt.legend(['Output', 'Input'])
+  if test != True:
+    d = pd.DataFrame({'Time': T, 'Input': u, 'Output': y})
+    return data_table.DataTable(d, include_index = False)
