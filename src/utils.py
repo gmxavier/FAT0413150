422141fa29e77aa8f4457d2ad0bacded0061323f
Gilberto M. Xavier <gilberto.xavier@lps.ufrj.br>
2022-04-16 12:18:58 -0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,6 @@
-import scipy as sp
-import numpy as np
-from control.matlab import *
 import matplotlib.pyplot as plt
-import pandas as pd
+from control.matlab import tf, feedback, lsim
+from pandas import DataFrame
 from google.colab import data_table
 data_table.enable_dataframe_formatter()
 
@@ -12,7 +10,7 @@ def pidtest(Kp, Ki, Kd,
             den   = [20**2, 2*0.5*20, 1],
             theta = 0,
             t0 = 25,
-            T  = np.arange(0, 10*20, 0.2),
+            T  = linspace(0, 10*20, 0.2),
             test = True):
   
   Gp = Gcl = tf(num, den)
@@ -21,15 +19,14 @@ def pidtest(Kp, Ki, Kd,
     Gc  = tf([Kd, Kp, Ki], [alpha*Kd/Kp, 1, 0])
     Gcl = feedback(Gc*Gp, 1)
     plt.title('Servo control response \n Kp = {0} | Ki = {1} | Kd = {2}'.format(Kp,Ki,Kd))
-  u = np.ones(len(T))
-  u[T<=t0] = 0
-  ud = np.ones(len(T))
-  ud[T<=(t0+theta)] = 0
+  u = 0*T
+  u[T>t0] = 1
+  ud[T>(t0+theta)] = 1
   y = lsim(Gcl, ud, T)[0]
   plt.plot(T, y, T, u)
   plt.xlabel('Time')
   plt.ylabel('Amplitude')
   plt.legend(['Output', 'Input'])
   if test != True:
-    d = pd.DataFrame({'Time': T, 'Input': u, 'Output': y})
+    d = DataFrame({'Time': T, 'Input': u, 'Output': y})
     return data_table.DataTable(d, include_index = False)
