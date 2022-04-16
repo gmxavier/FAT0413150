5ef1a67b167488937a333de212cd53740ee0b704
Gilberto M. Xavier <gilberto.xavier@lps.ufrj.br>
2022-04-16 12:34:17 -0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ def pidtest(Kp, Ki, Kd,
             den   = [20**2, 2*0.5*20, 1],
             theta = 0,
             t0 = 25,
-            T  = linspace(0, 10*20, 0.2),
+            tpar  = [0, 10*20, 0.2],
             test = True):
   
   Gp = Gcl = tf(num, den)
@@ -19,14 +19,15 @@ def pidtest(Kp, Ki, Kd,
     Gc  = tf([Kd, Kp, Ki], [alpha*Kd/Kp, 1, 0])
     Gcl = feedback(Gc*Gp, 1)
     plt.title('Servo control response \n Kp = {0} | Ki = {1} | Kd = {2}'.format(Kp,Ki,Kd))
-  u = 0*T
-  u[T>t0] = 1
-  ud[T>(t0+theta)] = 1
-  y = lsim(Gcl, ud, T)[0]
-  plt.plot(T, y, T, u)
+  time = linspace(tpar[0], tpar[1], tpar[2])
+  u = 0*time
+  u[time>t0] = 1
+  ud[time>(t0+theta)] = 1
+  y = lsim(Gcl, ud, time)[0]
+  plt.plot(time, y, time, u)
   plt.xlabel('Time')
   plt.ylabel('Amplitude')
   plt.legend(['Output', 'Input'])
   if test != True:
-    d = DataFrame({'Time': T, 'Input': u, 'Output': y})
+    d = DataFrame({'Time': time, 'Input': u, 'Output': y})
     return data_table.DataTable(d, include_index = False)
